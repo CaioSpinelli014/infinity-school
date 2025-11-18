afdf7b16da8833576452875524411b2d3ea79678
Caio Afonso <meunome4050@gmail.com>
2025-11-18 13:26:26 -0300

    Update dicionarios e sets.py

--- dicionarios e sets.py
+++ dicionarios e sets.py
@@ -1,41 +1,41 @@
-# frutas = set([])
-#
-# frutas_1 = {"maçã", "banana", "uva", "laranja", "morango"}
-#
-# frutas.update(frutas_1)
-# print(frutas)
-#
-# print(frutas.intersection("Banana"))
-#
-# frutas_vermelhas = {"morango", "cereja", "framboesa"}
-#
-# print(frutas_vermelhas)
-#
-# print(frutas_vermelhas.remove("cereja"))
-# print(frutas_vermelhas)
-# #################################################################
-# carro = {"fiat", "ford", "ferrari"}
-# moto = {"pop100", "gt1000", "yamara"}
-#
-# print(carro.union(moto))
-# ######################################################################
-# nome_1 = [1, 2, 3, 4]
-#
-# dados = {
-#     "Nome" : "Caio",
-#     "idade" : 20,
-#     "cidade" : "Recife"
-# }
-# for chave,valor in dados.items():
-#     print(f"Ola {chave}, {valor}")
-# #################################################################
-# dicionario = {}
-# lista = []
-#
-# lista = input("Digite uma lista: ")
-# dicionario = input("Digite um dicionario: ")
-#
-# print(lista in dicionario)
+frutas = set([])
+
+frutas_1 = {"maçã", "banana", "uva", "laranja", "morango"}
+
+frutas.update(frutas_1)
+print(frutas)
+
+print(frutas.intersection("Banana"))
+
+frutas_vermelhas = {"morango", "cereja", "framboesa"}
+
+print(frutas_vermelhas)
+
+print(frutas_vermelhas.remove("cereja"))
+print(frutas_vermelhas)
+#################################################################
+carro = {"fiat", "ford", "ferrari"}
+moto = {"pop100", "gt1000", "yamara"}
+
+print(carro.union(moto))
+######################################################################
+nome_1 = [1, 2, 3, 4]
+
+dados = {
+    "Nome" : "Caio",
+    "idade" : 20,
+    "cidade" : "Recife"
+}
+for chave,valor in dados.items():
+    print(f"Ola {chave}, {valor}")
+#################################################################
+dicionario = {}
+lista = []
+
+lista = input("Digite uma lista: ")
+dicionario = input("Digite um dicionario: ")
+
+print(lista in dicionario)
 
 contato = {}
 
@@ -68,6 +68,7 @@ for chave, valor in contato.items():
 
  for chave, valor in alunos.items():
      print(f"{chave}, {valor}")
+
 
 
 
